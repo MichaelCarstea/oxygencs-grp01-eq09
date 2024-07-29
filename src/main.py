f36b4b5e47dc2b789e6c39eed93c7c710358914e
allo-kun <mathieu.shea.1@ens.etsmtl.ca>
2024-07-29 19:44:13 -0400

    Fixed problems with database saving for the events and the temperatures

--- src/main.py
+++ src/main.py
@@ -1,13 +1,14 @@
-from signalrcore.hub_connection_builder import HubConnectionBuilder
-from dotenv import load_dotenv
 import logging
-import requests
 import json
 import time
 import os
+from signalrcore.hub_connection_builder import HubConnectionBuilder
+from dotenv import load_dotenv
+import requests
 import psycopg2
 
 load_dotenv()
+
 
 class App:
     def __init__(self):
@@ -16,14 +17,14 @@ class App:
         self.db_connection = self.init_db_connection()
 
         # To be configured by your team
-        self.HOST = os.getenv('HOST') # Utiliser les variables d'environnement
-        self.TOKEN = os.getenv('TOKEN')
-        self.T_MAX = os.getenv('T_MAX')
-        self.T_MIN = os.getenv('T_MIN')
-        self.DATABASE_URL = os.getenv('DATABASE_URL')
+        self.HOST = os.getenv("HOST")  # Utiliser les variables d'environnement
+        self.TOKEN = os.getenv("TOKEN")
+        self.T_MAX = os.getenv("T_MAX")
+        self.T_MIN = os.getenv("T_MIN")
+        self.DATABASE_URL = os.getenv("DATABASE_URL")
 
     def __del__(self):
-        if self._hub_connection != None:
+        if self._hub_connection is not None:
             self._hub_connection.stop()
 
     def start(self):
@@ -64,26 +65,25 @@ class App:
             timestamp = data[0]["date"]
             temperature = float(data[0]["data"])
             self.take_action(temperature)
-            self.save_event_to_database(timestamp, temperature)
+            self.save_temperature_to_database(timestamp, temperature)
         except Exception as err:
             print(err)
 
     def take_action(self, temperature):
         """Take action to HVAC depending on current temperature."""
         if float(temperature) >= float(self.T_MAX):
+            print("turnOnAC")
             self.send_action_to_hvac("TurnOnAc")
         elif float(temperature) <= float(self.T_MIN):
+            print("TurnOnHeater")
             self.send_action_to_hvac("TurnOnHeater")
+        print("In take action past ifs")
 
     def send_action_to_hvac(self, action):
         """Send action query to the HVAC service."""
-        r = requests.get(f"{self.HOST}/api/hvac/{self.TOKEN}/{action}/{self.TICKS}")
-        details = json.loads(r.text)
-        print(details, flush=True)
-        cursor = self.db_connection.cursor()
-        insert_query = "INSERT INTO sensor_event (action, timestamp) VALUES (%s, %s)"
-        cursor.execute(insert_query, (action) + (details["timestamp"], ))
-        self.db_connection.commit()
+        r = requests.get(f"{self.HOST}/api/hvac/{self.TOKEN}/{action}/{self.TICKS}", timeout=10)
+        json.loads(r.text)
+        self.save_event_to_database(action)
 
     def init_db_connection(self):
         try:
@@ -91,24 +91,37 @@ class App:
                 user="user01eq9",
                 password="Nenm1rpZpqHNRUyw",
                 host="157.230.69.113",
-                database="b01eq9"
+                database="db01eq9",
             )
             return connection
         except (Exception, psycopg2.Error) as error:
             print("Error while connecting to PostgreSQL", error)
+            return None
 
     def save_temperature_to_database(self, timestamp, temperature):
         """Save sensor data into database."""
         try:
             # To implement
             cursor = self.db_connection.cursor()
-            insert_query = "INSERT INTO sensor_data (timestamp, temperature) VALUES (%s, %s)"
+            insert_query = (
+                'INSERT INTO sensor_data ("Timestamp", "Temperature") VALUES (%s, %s)'
+            )
             cursor.execute(insert_query, (timestamp, temperature))
             self.db_connection.commit()
-            pass
         except (Exception, psycopg2.Error) as error:
-        print("Failed to insert record into table", error)
-            pass
+            print("Failed to insert record into table", error)
+
+    def save_event_to_database(self, event):
+        """Save sensor data into database."""
+        try:
+            cursor = self.db_connection.cursor()
+            insert_query = (
+                'INSERT INTO sensor_event ("Timestamp", "Event") VALUES (NOW(), %s)'
+            )
+            cursor.execute(insert_query, (event,))
+            self.db_connection.commit()
+        except (Exception, psycopg2.Error) as error:
+            print("Failed to insert record into table", error)
 
 
 if __name__ == "__main__":
